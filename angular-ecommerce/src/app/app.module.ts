a077c369224a15616448d206d27d324899b8e38a
Shanu Kumar <shanu.k.kumar@globallogic.com>
2022-01-03 13:01:56 +0530

    Release-2.0 Commmit | Pagintaion | master View

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -6,9 +6,16 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import {HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 const routes : Routes =[
-  {path:'category/:id',component:ProductListComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
+  {path: 'category/:id/:name', component: ProductListComponent},
   {path:'category',component:ProductListComponent},
   {path:'products',component:ProductListComponent},
   {path:'', redirectTo:'/products', pathMatch:'full'},
@@ -19,13 +26,16 @@ const routes : Routes =[
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent
   ],
   imports: [
-    RouterModule.forRoot(routes), 
+    RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
